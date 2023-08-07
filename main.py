010da855db594ad5df2974b46266d81d84beee8b
Dionne Bang <86373734+dbang21@users.noreply.github.com>
2023-08-07 18:03:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,106 +36,6 @@ auth0 = oauth.register(
     server_metadata_url=f'https://{DOMAIN}/.well-known/openid-configuration'
 )
 
-# NOT BING: START
-# This code is adapted from https://auth0.com/docs/quickstart/backend/python/01-authorization?_ga=2.46956069.349333901.1589042886-466012638.1589042885#create-the-jwt-validation-decorator
-class AuthError(Exception):
-    def __init__(self, error, status_code):
-        self.error = error
-        self.status_code = status_code
-
-
-@app.errorhandler(AuthError)
-def handle_auth_error(ex):
-    response = jsonify(ex.error)
-    response.status_code = ex.status_code
-    return response
-
-# Verify the JWT in the request's Authorization header
-def verify_jwt(request):
-    if 'Authorization' in request.headers:
-        auth_header = request.headers['Authorization'].split()
-        token = auth_header[1]
-    else:
-        raise AuthError({"code": "no auth header",
-                            "description":
-                                "Authorization header is missing"}, 401)
-    print("Authorization header exists")
-
-    jsonurl = urlopen("https://"+ DOMAIN+"/.well-known/jwks.json")
-    jwks = json.loads(jsonurl.read())
-    print("JWKS")
-    print(jwks)
-    try:
-        unverified_header = jwt.get_unverified_header(token)
-        print("UNVERIFIED_HEADER")
-        print(unverified_header)
-    except jwt.JWTError:
-        raise AuthError({"code": "invalid_header",
-                        "description":
-                            "Invalid header. "
-                            "Use an RS256 signed JWT Access Token"}, 401)
-    print("Authorization header valid")
-
-    if unverified_header["alg"] == "HS256":
-        raise AuthError({"code": "invalid_header",
-                        "description":
-                            "Invalid header. "
-                            "Use an RS256 signed JWT Access Token"}, 401)
-    print("Authorization header not HS256")
-
-    rsa_key = {}
-    for key in jwks["keys"]:
-        if key["kid"] == unverified_header["kid"]:
-            rsa_key = {
-                "kty": key["kty"],
-                "kid": key["kid"],
-                "use": key["use"],
-                "n": key["n"],
-                "e": key["e"]
-            }
-    if rsa_key:
-        try:
-            payload = jwt.decode(
-                token,
-                rsa_key,
-                algorithms=ALGORITHMS,
-                audience=CLIENT_ID,
-                issuer="https://"+ DOMAIN+"/"
-            )
-        except jwt.ExpiredSignatureError:
-            raise AuthError({"code": "token_expired",
-                            "description": "token is expired"}, 401)
-
-        except jwt.JWTClaimsError:
-            raise AuthError({"code": "invalid_claims",
-                            "description":
-                                "incorrect claims,"
-                                " please check the audience and issuer"}, 401)
-
-        except Exception:
-            raise AuthError({"code": "invalid_header",
-                            "description":
-                                "Unable to parse authentication"
-                                " token."}, 401)
-        
-        print("token not expired")
-        print("claims valid")
-        print("authentication parsed")
-
-        return payload
-    else:
-        print("no RSA key")
-        raise AuthError({"code": "no_rsa_key",
-                            "description":
-                                "No RSA key in JWKS"}, 401)
-    
-# Decode the JWT supplied in the Authorization header
-@app.route('/decode', methods=['GET'])
-def decode_jwt():
-    payload = verify_jwt(request)
-    return payload     
-# NOT BING: END
-
 
 @app.route("/login")
 def login():
@@ -146,14 +46,21 @@ def login():
 @app.route("/callback", methods=["GET", "POST"])
 def callback():
     token = oauth.auth0.authorize_access_token()
+    print("Token: ", token)
 
     sub = token['userinfo']['sub']
-    name = token['userinfo']['name']
     email = token['userinfo']['email']
-    picture = token['userinfo']['picture']
 
     # Fetch the updated user from the Datastore
     user = fetch_user(sub)
+
+    # If the user is not found in the Datastore, create a new user entity
+    if user is None:
+        user = {
+            "name": token['userinfo']['name'],
+            "email": email,
+            "picture": token['userinfo']['picture']
+        }
 
     # Update the session with the necessary user information
     session["user"] = {
@@ -163,7 +70,7 @@ def callback():
         "picture": user["picture"]  # Use the picture URL from the Datastore
     }
 
-    store_user(sub, name, email, picture)
+    store_user(sub, user["name"], email, user["picture"])
 
     return redirect("/")
 
@@ -230,13 +137,18 @@ def store_user(sub, name, email, picture):
     client.put(user_entity)  
 
 def fetch_user(sub):
-    query = client.query(kind='User')
+    query = client.query(kind=users_kind)
     query.add_filter('sub', '=', sub)
 
     # Convert to list
     users = list(query.fetch())   
 
-    return users[0]
+    if users:
+        # Return the first user found
+        return users[0]
+    else:
+        # Return None if no user is found
+        return None
 
 
 # CLAUDE (WEEK4)
@@ -245,7 +157,7 @@ def update_name(sub):
     
     print("Received sub:", sub)
 
-    query = client.query(kind='User')
+    query = client.query(kind=users_kind)
     query.add_filter('sub', '=', sub)
     user = list(query.fetch())[0]
     print(request.url)
@@ -256,7 +168,6 @@ def update_name(sub):
         print("IN POST NOW")
         name = request.form['name']
 
-        # chatgpt fixed claudes function (WEEK4)
         user['name'] = name  # Update the 'name' property directly
 
         # Save the changes to the Datastore using the 'put' method of the client
@@ -285,7 +196,7 @@ def update_picture(sub):
 
     print("Received sub:", sub)
 
-    query = client.query(kind='User')
+    query = client.query(kind=users_kind)
     query.add_filter('sub', '=', sub)
     user = list(query.fetch())[0]
     print(request.url)
@@ -321,8 +232,6 @@ def update_picture(sub):
         return render_template('update_picture.html', user=user)
 
 
-
-# updated by Claude to because deletion and page redirection wasn't working (WEEK 4)
 @app.route('/delete_account/<sub>', methods=['POST', 'GET'])
 def delete_account(sub):
     
@@ -330,33 +239,51 @@ def delete_account(sub):
 
     if request.method == 'POST':
 
-        query = client.query(kind='User')
+        query = client.query(kind=users_kind)
         query.add_filter('sub', '=', sub)
         user = list(query.fetch())[0]
         print("User:", user)
         print(request.url)
 
-        user_key = client.key(users_kind, sub)
-        print("User key:", user_key)
-
         if user is None:
-            return {'Error': 'No user with this sub exists'}, 404       
+            return {'Error': 'No user with this sub exists'}, 404     
+
+        user_key = user.key
+        print("User key:", user_key)  
         
         client.delete(user_key)
         
+        # Delete the user from Auth0
+        delete_user_from_auth0(sub)
+
         return redirect(url_for('logout'))
 
     else:
-    
         # GET request - Render confirmation page 
         return render_template('delete_confirmation.html', sub=sub)
 
-# CLAUDE (WEEK4)  
+def delete_user_from_auth0(user_id):
+    url = f"https://{DOMAIN}/api/v2/users/{user_id}"
+    headers = {
+        "Authorization": f"Bearer {get_auth0_access_token()}"
+    }
+    response = requests.delete(url, headers=headers)
+    if response.status_code == 204:
+        print("User successfully deleted from Auth0.")
+    else:
+        print("Failed to delete user from Auth0:", response.text)
+
+def get_auth0_access_token():
+    # Fetch an access token for the Auth0 Management API
+    response = auth0.fetch_access_token()
+    return response.get('access_token')
+
+  
 @app.route('/scores/<sub>')
 def scores(sub):
     return render_template('scores.html', sub=sub)
 
-# CLAUDE (WEEK4)  
+
 @app.route('/quizzes/<sub>')
 def quizzes(sub):
     return render_template('quizzes.html', sub=sub)
